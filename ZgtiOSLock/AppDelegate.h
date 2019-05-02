c38610101d4f24fc74e2f3006917dab31935d5de
gangtao.zhou <gangtao.zhou@ymm56.com>
2019-05-02 09:02:52 +0800

    Initial Commit

---
+++ ZgtiOSLock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZgtiOSLock
+//
+//  Created by gtzhou on 2019/5/2.
+//  Copyright © 2019 xyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
