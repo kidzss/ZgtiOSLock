978249a9a6d67a3118218ce076a7e9512b1a09d5
gangtao.zhou <gangtao.zhou@ymm56.com>
2019-05-02 20:34:42 +0800

    suo

--- ZgtiOSLock/ViewController.m
+++ ZgtiOSLock/ViewController.m
@@ -5,10 +5,26 @@
 //  Created by gtzhou on 2019/5/2.
 //  Copyright © 2019 xyz. All rights reserved.
 //
-
+#import <os/lock.h>
+#import <libkern/OSAtomic.h>
 #import "ViewController.h"
+#import <pthread.h>
 
 @interface ViewController ()
+{
+    OSSpinLock spinlock;
+    os_unfair_lock_t unfairLock;
+    NSInteger number;
+    dispatch_queue_t diapatchQueue;
+    CFTimeInterval end;
+    CFTimeInterval begin;
+    dispatch_semaphore_t semaphore;
+    
+    __block pthread_mutex_t mutex;
+    NSLock *lock;
+    NSRecursiveLock *recursiveLock;
+    NSConditionLock *conditionLock;// = [[NSConditionLock alloc] init];
+}
 
 @end
 
@@ -17,6 +33,192 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.title = @"iOS Lock";
+    spinlock = OS_SPINLOCK_INIT;
+    diapatchQueue = dispatch_queue_create("com.testiOSLock.queue", DISPATCH_QUEUE_CONCURRENT);
+    number = 500;
+    
+    unfairLock = &(OS_UNFAIR_LOCK_INIT);
+    semaphore = dispatch_semaphore_create(1);
+
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL);  // 定义锁的属性
+    
+    pthread_mutex_init(&mutex, &attr); // 创建锁
+    lock = [[NSLock alloc] init];
+    recursiveLock = [[NSRecursiveLock alloc] init];
+    conditionLock = [[NSConditionLock alloc] init];
+//    [self testSpinlock];
+//    [self testSemaphore];
+//    [self testMutex];
+//    [self testNSLock];
+//    [self testConditionLock];
+    [self testSynchronized];
+}
+
+- (void)testSpinlock {
+    begin = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < 1000; i++) {
+        dispatch_async(diapatchQueue, ^{
+            [self sellTicketSpinlock];
+        });
+    }
+}
+
+- (void)sellTicketSpinlock {
+    OSSpinLockLock(&spinlock);
+    if(number == 0) {
+        end = CFAbsoluteTimeGetCurrent();
+        NSLog(@"Spinlock test result %f",end - begin);
+        //Spinlock test result 0.016475
+    }
+    number--;
+    OSSpinLockUnlock(&spinlock);
+}
+
+- (void)testSemaphore {
+    begin = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < 1000; i++) {
+        dispatch_async(diapatchQueue, ^{
+            [self sellTicketSemaphore];
+        });
+    }
+}
+
+- (void)sellTicketSemaphore {
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    if(number == 0) {
+        end = CFAbsoluteTimeGetCurrent();
+        NSLog(@"Semaphore test result %f",end - begin);
+        //Semaphore test result 0.014200
+    }
+    number--;
+    dispatch_semaphore_signal(semaphore);
+}
+
+- (void)testMutex {
+    begin = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < 1000; i++) {
+        dispatch_async(diapatchQueue, ^{
+            [self sellTicketMutex];
+        });
+    }
+}
+
+- (void)sellTicketMutex {
+    pthread_mutex_lock(&mutex);
+    if(number == 0) {
+        end = CFAbsoluteTimeGetCurrent();
+        NSLog(@"Mutex test result %f",end - begin);
+        //Mutex test result 0.021771
+    }
+    number--;
+    pthread_mutex_unlock(&mutex);
+}
+
+- (void)testNSLock {
+    begin = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < 1000; i++) {
+        dispatch_async(diapatchQueue, ^{
+            [self sellTicketNSLock];
+        });
+    }
+}
+
+- (void)sellTicketNSLock {
+    [lock lock];
+    if(number == 0) {
+        end = CFAbsoluteTimeGetCurrent();
+        NSLog(@"NSLock test result %f",end - begin);
+        //NSLock test result 0.014743
+    }
+    number--;
+    [lock unlock];
+}
+
+- (void)testRecursiveLock {
+    begin = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < 1000; i++) {
+        dispatch_async(diapatchQueue, ^{
+            [self sellTicketRecursiveLock];
+        });
+    }
+}
+
+- (void)sellTicketRecursiveLock {
+    [recursiveLock lock];
+    if(number == 0) {
+        end = CFAbsoluteTimeGetCurrent();
+        NSLog(@"RecursiveLock test result %f",end - begin);
+        //RecursiveLock test result 0.020444
+    }
+    number--;
+    [recursiveLock unlock];
+}
+
+- (void)testConditionLock {
+    begin = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < 1000; i++) {
+        dispatch_async(diapatchQueue, ^{
+            [self sellTicketConditionLock];
+        });
+    }
+}
+
+//conditionLock 场合不合适
+- (void)sellTicketConditionLock {
+    [conditionLock lock];
+    
+    if(number == 0) {
+        end = CFAbsoluteTimeGetCurrent();
+        NSLog(@"ConditionLock test result %f",end - begin);
+        //ConditionLock test result 0.022440
+    }
+    number--;
+    [conditionLock unlock];
+}
+
+- (void)testSynchronized {
+    begin = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < 1000; i++) {
+        dispatch_async(diapatchQueue, ^{
+            [self sellTicketSynchronized];
+        });
+    }
+}
+
+//conditionLock 场合不合适
+- (void)sellTicketSynchronized {
+    @synchronized (self) {
+        if(number == 0) {
+            end = CFAbsoluteTimeGetCurrent();
+            NSLog(@"Synchronized test result %f",end - begin);
+            //ConditionLock test result 0.022440
+        }
+        number--;
+    }
+}
+
+//不支持并发模式
+- (void)testUnfairLock {
+    begin = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < 1000; i++) {
+        dispatch_async(diapatchQueue, ^{
+            [self sellTicketUnfairLock];
+        });
+    }
+}
+//
+- (void)sellTicketUnfairLock {
+    os_unfair_lock_lock(unfairLock);
+    if(number == 0) {
+        end = CFAbsoluteTimeGetCurrent();
+        NSLog(@"UnfairLock test result %f",end - begin);
+        //start end - begin 0.016532
+    }
+    number--;
+    os_unfair_lock_unlock(unfairLock);
 }
 
 
